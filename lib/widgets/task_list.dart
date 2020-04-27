143174daf80007176466f8b30523fa69279b5fd6
fadywaheed <fadywaheed10@gnail.com>
2020-04-27 23:31:44 +0200

    added models

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:todey/models/task.dart';
 import 'package:todey/widgets/task_tile.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTile: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkBoxState:(bool checkboxState){
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          }
+        );
+      },
     );
   }
 }
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
 
+  final bool isChecked;
+  final String taskTile;
+  final Function checkBoxState;
+  TaskTile({this.isChecked, this.taskTile,this.checkBoxState});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('My first task.'),
-      trailing: Checkbox(value: false),
+        title: Text(
+          taskTile,
+          style: TextStyle(
+              fontWeight: FontWeight.w500,
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Checkbox(
+          value: isChecked,
+          activeColor: Colors.lightBlueAccent,
+          onChanged: checkBoxState,
+        )
     );
   }
 }
